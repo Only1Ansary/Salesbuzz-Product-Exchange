29a360c9ccf6d00170aff84a680203a4c606e7e1
Only1Ansary <ahmedalansary136@gmail.com>
2026-08-23 23:31:27 +0300

    Second

---
+++ SalesBuzzProductExchangeApi/Data/ProductExchangeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesBuzzProductExchangeApi.Models;
+using SalesBuzz.Shared.Data;
+
+namespace SalesBuzzProductExchangeApi.Data;
+
+public class ProductExchangeDbContext : SalesBuzzDbContextBase
+{
+    public ProductExchangeDbContext(DbContextOptions options, ICurrentBUContext buContext)
+        : base(options, buContext) { }
+
+    public DbSet<ProductExchangeOrder> ProductExchangeOrders { get; set; }
+}
---
+++ SalesBuzzProductExchangeApi/Migrations/20260822121120_InitialCreate.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SalesBuzzProductExchangeApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductExchangeOrders",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OriginalProduct = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OriginalQuantity = table.Column<int>(type: "int", nullable: false),
+                    ReplacementProduct = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ReplacementQuantity = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductExchangeOrders", x => x.OrderId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductExchangeOrders");
+        }
+    }
+}
